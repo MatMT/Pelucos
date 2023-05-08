73e1523eed80f1c97f65c1294dd69cfdf9a1c0e1
Mateo Elías <oscarmateoelias@gmail.com>
2023-05-08 17:36:02 -0600

    Conexión de API LAravel

--- resources/views/templates/barra.blade.php
+++ resources/views/templates/barra.blade.php
@@ -9,8 +9,8 @@
 
 @if ($auth->admin)
     <div class="barra-servicios">
-        <a href="/admin" class="boton">Ver Citas</a>
-        <a href="/servicios" class="boton">Ver Servicios</a>
-        <a href="/servicios/crear" class="boton">Nuevo Servicio</a>
+        <a href="{{ route('citas') }}" class="boton">Ver Citas</a>
+        <a href="{{ route('servicios') }}" class="boton">Ver Servicios</a>
+        <a href="{{}}" class="boton">Nuevo Servicio</a>
     </div>
 @endif
